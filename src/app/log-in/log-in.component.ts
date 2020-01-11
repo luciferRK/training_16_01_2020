1875204d9dafe42136a2bf87538a1603c892c544
luciferRK <likhithr.cs17@sahyadri.edu.in>
2020-01-11 13:25:41 +0530

    forget password

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import * as core from '@angular/core';
 
-@Component({
+@core.Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.css']
 })
-export class LogInComponent implements OnInit {
+export class LogInComponent implements core.OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  popupLink(){
+    let modal: HTMLElement = document.getElementById("forgot_password");
+    modal.setAttribute("style","display:block");
+    let elem: HTMLElement = document.getElementById("over");
+    elem.setAttribute("style","display:block");
+  }
 }
