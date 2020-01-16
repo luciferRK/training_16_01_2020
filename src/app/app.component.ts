a09cb38296bbf72f9192b4bc610673f197c30c66
luciferRK <likhithr.cs17@sahyadri.edu.in>
2020-01-16 09:57:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Shopee';
+  ngOnInit(){
+
+  }
+
+  imgname = "logo"   //image name without extension use the vraiable in image tag to use it
+
+
+  changeImage(){
+    if(this.imgname=="logo"){
+      this.imgname="img-01"
+    }
+    else{
+      this.imgname="logo"
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,16 @@
-import { SignUpComponent_2 } from './sign-up/sign-up_2.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { LogInComponent } from './log-in/log-in.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PwdComponent } from './pwd/pwd.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
-    SignUpComponent,
-    SignUpComponent_2,
-    LogInComponent,
-    PwdComponent
   ],
   imports: [
     BrowserModule,
